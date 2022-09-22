f8dcf17b891335ae98a6552afda8539415a03539
Timothy Kiyeng <timothy.kiyeng@student.moringaschool.com>
2022-09-22 13:10:30 +0300

    Added useState

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
+
+
+function handleClick(){
+  return 
+}
+
+
+
 
 function Toggle() {
-  return <button>OFF</button>;
+const [isOn, setIsOn] = useState(false)
+
+  return <button onClick={handleClick}>{isOn ? "ON" : "OFF"}</button>
+
 }
+
+
 
 export default Toggle;
